a80c7c64a0fd2b12745d6bf7f2e2aff2f91adb5e
Sorimoutou <sori.sully@gmail.com>
2022-03-12 20:27:27 +0100

    [Banner-component] finished

--- src/components/banner-component.vue
+++ src/components/banner-component.vue
@@ -1,30 +1,35 @@
 <template>
-    <section class="elements">
+<div>
+    <section v-if="mode=='square' " class="elements">
         <div class="element" v-for="element in item" :key="element.index" >
 
             <!-- Square mode -->
-            <a  class="element" v-if="squareChecking && element.type=='image'" :href="element.link">
+            <a  class="element" v-if="element.type=='image'" :href="element.link">
                 <img class="element-image" alt="couch from Butopea website" :src="element.pictureUrl" >
             </a>
 
-            <a v-else-if =" squareChecking && element.type == 'cta'" tesfunction class="element-cta" :href="element.link"> 
+            <a v-else-if =" element.type == 'cta'" tesfunction class="element-cta" :href="element.link"> 
                 <h2>{{element.title}}</h2>
-                <button>{{element.button}}</button>
+                <button :href="element.link">{{element.button}}</button>
             </a>
-
-            <!-- Rectangle mode -->
-            <a  class="element" v-if="rectangleChecking && element.type=='image'" :href="element.link">
-                <img class="element-image" alt="couch from Butopea website" :src="element.pictureUrl" >
-            </a>
-             <a v-else-if =" rectangleChecking && element.type == 'cta'" tesfunction class="element-cta" :href="element.link"> 
-                <h2>{{element.title}}</h2>
-                <button>{{element.button}}</button>
-            </a>
-    
 
       </div>
     </section>
-    
+
+    <section v-else class=" rectangle elements ">
+         <div class="element" v-for="element in item" :key="element.index" >
+            <!-- Rectangle mode -->
+            <a  class="element-rectangle" v-if="element.type=='image'" :href="element.link">
+                <img class="element-image " alt="couch from Butopea website" :src="element.pictureUrl" >
+            </a>
+
+             <a v-else-if ="element.type == 'cta'" tesfunction class="opacity element-cta " :href="element.link"> 
+                <h2>{{element.title}}</h2>
+                <button :href="element.link">{{element.button}}</button>
+            </a>
+      </div>
+    </section>
+</div>   
 </template>
 
 <script>
@@ -35,31 +40,44 @@ export default {
         carousel : {type: Boolean, required: false},
         item : {type: Array, required: true,}
 	},
-    computed: {
-        squareChecking(){
-            return this.mode=='square';
-        },
-        rectangleChecking(){
-            return this.mode =='rectangle'
-        }
-    }
 }
 </script>
 
 <style>
-
+a{
+    text-decoration: none;
+}
+h2{
+    font-family: 'Barlow Condensed';
+    font-size: 48px;
+    color: white;
+}
+button{
+    background-color: unset;
+    border: 1px solid white;
+    color: white;
+    padding: 25px;
+    padding: 16px 32px;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 600;
+    cursor: pointer;
+}
 .elements{
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: center;
     flex-wrap: wrap;
     margin-block: 50px;
+    padding-inline: 180px;
+    width: fit-content;
+
 }
 .element{
     width: 500px;
     height: 500px;
     display: flex;
+    border: 1px solid #42f5c2;
 }
 .element-image{
     object-fit: cover;
@@ -70,12 +88,32 @@ export default {
 .element-cta{
     width: 100%;
     height: 100%;
-    background-color: lightcyan;
+    background-color: #42f5c2;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
 }
-
+.opacity{
+    background-color: rgba(255,255,255 0.5);
+}
+.element-rectangle{
+    width: 1500px;
+    height: 500px;
+}
+.opacity{
+    background-color: rgba(255,255,255,0.8);
+}
+.opacity h2 {
+    color: #42f5c2;
+}
+.opacity button{
+    color: #42f5c2;
+    border: 1px solid #42f5c2;
+}
+.rectangle {
+    justify-content: space-between;
+    width: auto;
+}
 
 </style>
